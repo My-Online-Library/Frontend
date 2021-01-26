72c1d5f6be572c631f93d498851ace0728c644da
Nestor De La Cruz <ndelacruz@evopointsolution.com>
2021-01-25 23:39:31 -0400

    Instalado Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorListComponent } from './author/author-list/author-list.component';
 import { ListComponent } from './utils/list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ListComponent } from './utils/list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent,]
